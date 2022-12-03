ec1eb5aee9fc7461ecfabcca534ce7d1f5868d7f
Jakye <jakyeru@gmail.com>
2022-12-03 15:47:39 +0200

    Added steam-session.js

---
+++ src/steam-session.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const consoleHelper = require('./helpers/console');
+const qrCodeTerminal = require('qrcode-terminal');
+const { LoginSession, EAuthTokenPlatformType } = require('steam-session');
+
+const session = new LoginSession(EAuthTokenPlatformType.SteamClient);
+
+module.exports = new Promise(async (resolve, reject) => {
+    const qrCodeData = await session.startWithQR().catch(reject);
+
+    consoleHelper.info('QR Code generated. Please scan it with the Steam Mobile Authenticator app.');
+
+    qrCodeTerminal.generate(qrCodeData.qrChallengeUrl, { small: true });
+
+    session.on('authenticated', () => {
+        return resolve(session);
+    });
+
+    session.on('timeout', () => {
+        return reject('Timed out. Please try again.');
+    });
+
+    session.on('error', error => {
+        if (error.eresult === 9) return reject('Login cancelled from Steam Mobile Authenticator app. Please try again.');
+
+        return reject(error);
+    });
+});
